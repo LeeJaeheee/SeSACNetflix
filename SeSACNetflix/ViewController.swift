d2c3b2ba6b76652fe500b2ae17d23f9e8b9459c0
LeeJaeheee <sns3434@gmail.com>
2024-01-26 21:24:38 +0900

    Initial Commit

---
+++ SeSACNetflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACNetflix
+//
+//  Created by 이재희 on 1/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
